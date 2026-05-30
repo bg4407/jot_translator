05d6cad07c0e62b7b2ce52a37d972c241818e835
Jatin <121313000+itzme170605@users.noreply.github.com>
2026-05-30 00:50:19 +0000

    just some more code

--- project_files/provided/JottTokenizer.java
+++ project_files/provided/JottTokenizer.java
@@ -14,79 +14,136 @@ import java.io.FileNotFoundException;
 
 public class JottTokenizer {
 
-	/**
+    /**
      * Takes in a filename and tokenizes that file into Tokens
      * based on the rules of the Jott Language
      * @param filename the name of the file to tokenize; can be relative or absolute path
      * @return an ArrayList of Jott Tokens
      */
-  public static ArrayList<Token> tokenize(String filename){
-    ArrayList<Token> mylist = new ArrayList<Token>();
-    File file = new File(filename);
-    try(Scanner reader = new Scanner(file);){
-      int lineNum = 0;
-      while(reader.hasNextLine()){
-        lineNum++;
-        String line = reader.nextLine().strip();
-        for(int i = 0; i < line.length(); i++){
-          switch(line.charAt(i)){
-            case '\n':
-            case '\r':
-              lineNum++;
-            case '\t':
-            case ' ':
-              break;
-            case '#':
-              //basically breaks the for loop
-              i = line.length();
-              break;
-            case ',':
-              mylist.add(new Token(",", filename, lineNum, TokenType.COMMA));
-              break;
-            case ']':
-              mylist.add(new Token("]", filename, lineNum, TokenType.R_BRACKET));
-              break;
-            case '[':
-              mylist.add(new Token("[", filename, lineNum, TokenType.L_BRACKET));
-              break;
-            case '}':
-              mylist.add(new Token("}", filename, lineNum, TokenType.R_BRACE));
-              break;
-            case '{':
-              mylist.add(new Token("{", filename, lineNum, TokenType.L_BRACE));
-              break;
-            case '/':
-              mylist.add(new Token("/", filename, lineNum, TokenType.MATH_OP));
-              break;
-            case '+':
-              mylist.add(new Token("+", filename, lineNum, TokenType.MATH_OP));
-              break;
-            case '-':
-              mylist.add(new Token("-", filename, lineNum, TokenType.MATH_OP));
-              break;
-            case '*':
-              mylist.add(new Token("*", filename, lineNum, TokenType.MATH_OP));
-              break;
-            case ';':
-              mylist.add(new Token(";", filename, lineNum, TokenType.SEMICOLON));
-              break;
-            case '':
-              break;
-            case '':
-              break;
-            
-          }
-        }
-        
+    public static ArrayList<Token> tokenize(String filename) {
+        ArrayList<Token> mylist = new ArrayList<Token>();
+        File file = new File(filename);
+        int lineNum = 0;
 
-      }
-    } catch(FileNotFoundException e){
-      System.err.print(e);
-      return null;
+        try (Scanner reader = new Scanner(file);) {
+            while (reader.hasNextLine()) {
+                lineNum++;
+                String line = reader.nextLine().strip(); 
+
+                for (int i = 0; i < line.length(); i++) {
+                    char currentChar = line.charAt(i);
+
+                    switch (currentChar) {
+                        case '\t':
+                        case ' ':
+                        case '\r':
+                            break;
+                        case '#':
+                            i = line.length();
+                            break;
+                        case ',':
+                            mylist.add(new Token(",", filename, lineNum, TokenType.COMMA));
+                            break;
+                        case ']':
+                            mylist.add(new Token("]", filename, lineNum, TokenType.R_BRACKET));
+                            break;
+                        case '[':
+                            mylist.add(new Token("[", filename, lineNum, TokenType.L_BRACKET));
+                            break;
+                        case '}':
+                            mylist.add(new Token("}", filename, lineNum, TokenType.R_BRACE));
+                            break;
+                        case '{':
+                            mylist.add(new Token("{", filename, lineNum, TokenType.L_BRACE));
+                            break;
+                        case ';':
+                            mylist.add(new Token(";", filename, lineNum, TokenType.SEMICOLON));
+                            break;
+                        case '/':
+                            mylist.add(new Token("/", filename, lineNum, TokenType.MATH_OP));
+                            break;
+                        case '+':
+                            mylist.add(new Token("+", filename, lineNum, TokenType.MATH_OP));
+                            break;
+                        case '-':
+                            mylist.add(new Token("-", filename, lineNum, TokenType.MATH_OP));
+                            break;
+                        case '*':
+                            mylist.add(new Token("*", filename, lineNum, TokenType.MATH_OP));
+                            break;
+
+                        // Header Lookahead (: vs ::)
+                        case ':':
+                            if (i + 1 < line.length() && line.charAt(i + 1) == ':') {
+                                mylist.add(new Token("::", filename, lineNum, TokenType.FC_HEADER));
+                                i++; // Skip lookahead char
+                            } else {
+                                mylist.add(new Token(":", filename, lineNum, TokenType.COLON));
+                            }
+                            break;
+                        case '=':
+                            if (i + 1 < line.length() && line.charAt(i + 1) == '=') {
+                                mylist.add(new Token("==", filename, lineNum, TokenType.REL_OP));
+                                i++;
+                            } else {
+                                mylist.add(new Token("=", filename, lineNum, TokenType.ASSIGN));
+                            }
+                            break;
+                        case '<':
+                            if (i + 1 < line.length()) {
+                                char nextChar = line.charAt(i + 1);
+                                if (nextChar == '=') {
+                                    mylist.add(new Token("<=", filename, lineNum, TokenType.REL_OP));
+                                    i++;
+                                } else if (nextChar == '>') {
+                                    mylist.add(new Token("<>", filename, lineNum, TokenType.REL_OP));
+                                    i++;
+                                } else {
+                                    mylist.add(new Token("<", filename, lineNum, TokenType.REL_OP));
+                                }
+                            } else {
+                                mylist.add(new Token("<", filename, lineNum, TokenType.REL_OP));
+                            }
+                            break;
+
+                        case '>':
+                            if (i + 1 < line.length() && line.charAt(i + 1) == '=') {
+                                mylist.add(new Token(">=", filename, lineNum, TokenType.REL_OP));
+                                i++;
+                            } else {
+                                mylist.add(new Token(">", filename, lineNum, TokenType.REL_OP));
+                            }
+                            break;
+
+                        case '!':
+                            if (i + 1 < line.length() && line.charAt(i + 1) == '=') {
+                                mylist.add(new Token("!=", filename, lineNum, TokenType.REL_OP));
+                                i++;
+                            } else {
+                                System.err.println("Syntax Error:");
+                                System.err.println("Invalid token \"!\". \"!\" expects following \"=\"");
+                                System.err.println(filename + ":" + lineNum);
+                                return null;
+                            }
+                            break;
+
+                        // String literals scanning logic
+                        case '"':
+                            // TODO: implement string literal scanning (stubbed intentionally)
+                            break;
+
+                        default:
+                            // Multi-character sequences (identifiers, keywords, numbers, etc.)
+                            // Scanning logic is intentionally left as a stub.
+                            break;
+                    }
+                }
+            }
+        } catch (FileNotFoundException e) {
+            System.err.print(e);
+            return null;
+        }
+
+        return mylist; // Returns your parsed collection safely!
     }
-    
-  
-		return null;
-	}
-  
 }
